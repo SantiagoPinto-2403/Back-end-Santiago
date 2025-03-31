2fa12ce7e5c430b6938ebf79a4009f00c5ccb55b
SantiagoPinto-2403 <santiago.pintod@urosario.edu.co>
2025-03-31 12:54:56 -0500

    Create service.py

---
+++ app/service.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, HTTPException, Request
+import uvicorn
+from app.controlador.PatientCrud import GetServiceRequestById,WriteService,GetServiceRequestByPatiIdentifier
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://patient-santiago.onrender.com"],  # Permitir solo este dominio
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
+
+@app.get("/patient/{patient_id}", response_model=dict)
+async def get_patient_by_id(patient_id: str):
+    status,patient = GetPatientById(patient_id)
+    if status=='success':
+        return patient  # Return patient
+    elif status=='notFound':
+        raise HTTPException(status_code=204, detail="El paciente no existe")
+    else:
+        raise HTTPException(status_code=500, detail=f"Error interno del servidor. {status}")
+
+@app.post("/patient", response_model=dict)
+async def add_patient(request: Request):
+    new_patient_dict = dict(await request.json())
+    status,patient_id = WritePatient(new_patient_dict)
+    if status=='success':
+        return {"_id":patient_id}  # Return patient id
+    else:
+        raise HTTPException(status_code=500, detail=f"Error interno del servidor: {status}")
+
+@app.get("/patient", response_model=dict)
+async def get_patient_by_identifier(system: str, value: str):
+    status,patient = GetPatientByIdentifier(system,value)
+    if status=='success':
+        return patient  # Return patient
+    elif status=='notFound':
+        raise HTTPException(status_code=204, detail="El paciente no existe")
+    else:
+        raise HTTPException(status_code=500, detail=f"Error interno del servidor. {status}")
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
