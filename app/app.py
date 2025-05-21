0fb5b316c4fadd81451fc96862c8087e1f000efd
SantiagoPinto-2403 <santiago.pintod@urosario.edu.co>
2025-05-21 17:11:39 -0500

    y

--- app/app.py
+++ app/app.py
@@ -3,14 +3,14 @@ import uvicorn
 from app.controlador.PatientCrud import GetPatientById,WritePatient,GetPatientByIdentifier
 from app.controlador.ServiceRequestCrud import GetServiceRequestById, WriteServiceRequest, GetServiceRequestByIdentifier
 from app.controlador.AppointmentCrud import GetAppointmentById, WriteAppointment, GetAppointmentByIdentifier
-from app.controlador.DiagnosticReportCrud import GetDiagnosticReportById, WriteDiagnosticReport, GetDiagnosticReportByIdentifier
+#from app.controlador.DiagnosticReportCrud import GetDiagnosticReportById, WriteDiagnosticReport, GetDiagnosticReportByIdentifier
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://patient-santiago.onrender.com","https://servicerequest.onrender.com","https://appointment-em7f.onrender.com","https://diagnosticreport.onrender.com/"],  # Permitir solo este dominio
+    allow_origins=["https://patient-santiago.onrender.com","https://servicerequest.onrender.com","https://appointment-em7f.onrender.com"],  # Permitir solo este dominio
     allow_credentials=True,
     allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos los encabezados
@@ -108,39 +108,6 @@ async def get_appointment_by_identifier(system: str, value: str):
         raise HTTPException(status_code=204, detail="Appointment not found")
     else:
         raise HTTPException(status_code=500, detail=f"Internal server error. {status}")
-
-##DIAGNOSTIC REPORT 
-
-@app.get("/diagnosticreport/{report_id}", response_model=dict)
-async def get_diagnostic_report_by_id(report_id: str):
-    status, diagnostic_report = GetDiagnosticReportById(report_id)  
-    if status == 'success':
-        return diagnostic_report 
-    elif status == 'notFound':
-        raise HTTPException(status_code=204, detail="El informe diagnóstico no existe") 
-    else:
-        raise HTTPException(status_code=500, detail=f"Error interno del servidor. {status}")
-
-
-@app.post("/diagnosticreport", response_model=dict)
-async def add_diagnostic_report(request: Request):
-    new_diagnostic_report_dict = dict(await request.json()) 
-    status, report_id = WriteDiagnosticReport(new_diagnostic_report_dict)  
-    if status == 'success':
-        return {"_id": report_id}  
-    else:
-        raise HTTPException(status_code=500, detail=f"Error interno del servidor: {status}") 
-
-
-@app.get("/diagnosticreport", response_model=dict)
-async def get_diagnostic_report_by_identifier(system: str, value: str):
-    status, diagnostic_report = GetDiagnosticReportByIdentifier(system, value) 
-    if status == 'success':
-        return diagnostic_report  
-    elif status == 'notFound':
-        raise HTTPException(status_code=204, detail="El informe diagnóstico no existe")
-    else:
-        raise HTTPException(status_code=500, detail=f"Error interno del servidor. {status}")
 
 if __name__ == '__main__':
     import uvicorn
